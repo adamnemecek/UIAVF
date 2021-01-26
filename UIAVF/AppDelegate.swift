0ff6e5641bbce304cea40502bba4f0b10eb3cdd7
adamnemecek <adamnemecek@gmail.com>
2021-01-26 12:09:35 -0800

    stuff

---
+++ UIAVF/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UIAVF
+//
+//  Created by Adam Nemecek on 1/26/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
