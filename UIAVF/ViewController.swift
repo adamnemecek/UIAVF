28e681f970b01bebf5ce285fa076039bc56ae2de
adamnemecek <adamnemecek@gmail.com>
2021-01-26 12:15:22 -0800

    stuff

--- UIAVF/ViewController.swift
+++ UIAVF/ViewController.swift
@@ -6,11 +6,42 @@
 //
 
 import Cocoa
+import AVFoundation
+import CoreAudioKit
+
+func stuff() {
+    let a = AVAudioUnitComponentManager.shared().components(passingTest: {
+        if $0.name.contains("Sylenth") {
+            $1.pointee = true
+            return true
+        }
+        else {
+            return false
+        }
+    })
+    
+    let desc = a.first!.audioComponentDescription
+    let instrument = AVAudioUnitMIDIInstrument(audioComponentDescription: desc)
+    print("instrument \(instrument)")
+    instrument.auAudioUnit.requestViewController(completionHandler: {
+        print($0)
+    })
+//    AVAudioUnit.instantiate(with: desc, options: [], completionHandler: { (unit, err) in
+//        Swift.print("instantiated unit")
+//        print(err)
+//        unit!.auAudioUnit.requestViewController(completionHandler: { (c) in
+//            print("inner")
+//        })
+//    })
+    
+}
+
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        stuff()
 
         // Do any additional setup after loading the view.
     }
